d1bcce70b45ac8dd5d3367391a74f06acdaf2669
aanmeba <aanmeba@gmail.com>
2023-08-30 16:09:09 +1000

    add test configuration

--- src/test/java/io/nology/employeecreator/employee/EmployeeControllerTest.java
+++ src/test/java/io/nology/employeecreator/employee/EmployeeControllerTest.java
@@ -3,7 +3,11 @@ package io.nology.employeecreator.employee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(
+		properties = "test.base-url=http://localhost:8080",
+		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EmployeeControllerTest {
 
 	
