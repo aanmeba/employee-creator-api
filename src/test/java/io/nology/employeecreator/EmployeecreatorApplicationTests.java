520cfc81864120cb52e71d0d4467ecbac36c9855
aanmeba <aanmeba@gmail.com>
2023-08-30 15:49:00 +1000

    add default test

--- src/test/java/io/nology/employeecreator/EmployeecreatorApplicationTests.java
+++ src/test/java/io/nology/employeecreator/EmployeecreatorApplicationTests.java
@@ -1,7 +1,10 @@
 package io.nology.employeecreator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -9,7 +12,8 @@ import org.springframework.test.context.ActiveProfiles;
 class EmployeecreatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
