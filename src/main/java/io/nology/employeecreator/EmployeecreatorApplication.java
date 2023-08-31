ff48109f9b515bb56e5413dd191678c4f4d83f52
aanmeba <aanmeba@gmail.com>
2023-08-31 17:03:17 +1000

    add environment setup and @CrossOrigin annotation for network

--- src/main/java/io/nology/employeecreator/EmployeecreatorApplication.java
+++ src/main/java/io/nology/employeecreator/EmployeecreatorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeecreatorApplication {
 
 	public static void main(String[] args) {
+		EnvLoader.load();
 		SpringApplication.run(EmployeecreatorApplication.class, args);
 	}
 
